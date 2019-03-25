61daa2cfb8e9b0587c949e62a24c0c1547fbe8fd
pmbaile3 <pmbaile3@ncsu.edu>
2019-03-25 16:32:04 -0400

    Added file transfer over UDP

--- 401_proj2/src/processes/Client.java
+++ 401_proj2/src/processes/Client.java
@@ -1,5 +1,42 @@
-package processes;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Client {
+    
+    private static int MSS = 100;
+
+    public static void main ( String[] args ) {
+        try {
+            final DatagramSocket socket = new DatagramSocket( );
+            final InetAddress address = InetAddress.getByName( "localhost" );
+            byte[] b = new byte[MSS];
+            final InputStream is = new FileInputStream( "fileToRead.txt" );
+
+            int readBytes = 0;
+            b[0] = 't';
+            b[1] = 't';
+            b[2] = 't';
+            b[3] = 't';
+            b[4] = 't';
+            b[5] = 't';
+            b[6] = 't';
+            b[7] = 't';
+            while ( ( readBytes = is.read( b, 8, MSS - 8 ) ) != -1 ) {
+                final DatagramPacket packet = new DatagramPacket( b, readBytes + 8, address, 7735 );
+                socket.send( packet );
+                System.out.println(readBytes + 8);
+            }
+            is.close();
+            socket.close();
+
+        }
+        catch ( final IOException ioe ) {
+            System.out.println( "Error " + ioe.getMessage() );
+        }
+    }
 
 }
--- 401_proj2/src/processes/Server.java
+++ 401_proj2/src/processes/Server.java
@@ -1,5 +1,37 @@
-package processes;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Arrays;
 
 public class Server {
+
+    public static void main ( String[] args ) {
+        try {
+            final DatagramSocket socket = new DatagramSocket( 7735 );
+            final InetAddress address = InetAddress.getByName( "localhost" );
+            final byte[] b = new byte[1000];
+            final OutputStream os = new FileOutputStream( "fileToPrint.txt" );
+
+            final int readBytes = 0;
+            boolean running = true;
+            while ( running ) {
+                final DatagramPacket packet = new DatagramPacket( b, b.length );
+                socket.receive( packet );
+                os.write( Arrays.copyOfRange( packet.getData(), 8, packet.getLength() ) );
+                if( b.length == 0) {
+                    running = false;
+                }
+            }
+            os.close();
+            socket.close();
+
+        }
+        catch ( final IOException ioe ) {
+            System.out.println( "Error " + ioe.getMessage() );
+        }
+    }
 
 }
